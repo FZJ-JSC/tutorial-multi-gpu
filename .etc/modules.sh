5860349481f80e6d2f86b1f33b15b6859538f812
Andreas Herten <a.herten@fz-juelich.de>
2022-05-16 13:31:53 +0200

    Fix NCCL version in modules

--- .etc/modules.sh
+++ .etc/modules.sh
@@ -5,6 +5,6 @@ module load GCC/11.2.0
 module load CUDA/11.5
 module load ParaStationMPI/5.5.0-1
 module load NVSHMEM/2.5.0
-module load NCCL/2.10.3-1-CUDA-11.3
+module load NCCL/2.12.7-1-CUDA-11.5
 module load Nsight-Systems/2022.2.1
 # module use $JSCCOURSE_DIR_GROUP/common/modulefiles

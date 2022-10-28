d89f072b4aa06bf1357359930a44299ab1308c7a
Andreas Herten <a.herten@fz-juelich.de>
2022-10-28 14:45:44 +0200

    Update modules for 2022, using Stages/2023

--- .etc/modules.sh
+++ .etc/modules.sh
@@ -1,10 +1,10 @@
-module use $OTHERSTAGES
 module purge
-module load Stages/2022
-module load GCC/11.2.0
-module load CUDA/11.5
-module load ParaStationMPI/5.5.0-1
+module load Stages/2023
+module load GCC/11.3.0
+module load CUDA/11.7
+module load ParaStationMPI/5.6.0-1
+module load NCCL/default-CUDA-11.7
 module load NVSHMEM/2.5.0
-module load NCCL/2.12.7-1-CUDA-11.5
-module load Nsight-Systems/2022.2.1
+module load Nsight-Systems/2022.4.1
+module load MPI-settings/CUDA
 # module use $JSCCOURSE_DIR_GROUP/common/modulefiles

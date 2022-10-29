5ab95ddf524247a44a163737f7ce331bb21beac9
Andreas Herten <a.herten@fz-juelich.de>
2022-10-29 11:08:40 +0200

    Update archive script

--- .archive.mk
+++ .archive.mk
@@ -2,18 +2,20 @@
 # For JSC Courses
 # Generates a tar from all top-level directory in this current folder, without hidden files
 # -Andreas Herten, 2021 April 27
+#
+# Changelog:
+# * Nov 2022: The archive is extracted again, then slides.pdf is removed if a patched slides-sc22.pdf is found (which includes an SC22 slide 0 title slide); and then repackaged
 .PHONY: all
 all: tut102-multi-gpu.tar.gz
 
 SOURCES=$(shell gfind . -maxdepth 1 -mindepth 1 -not -path "./.*" -not -name "tut102-multi-gpu.tar.gz" -printf '%P\n' | sort -h)
 
 tut102-multi-gpu.tar.gz: $(shell find . -not -name "tut102-multi-gpu.tar.gz")
-# 	if ! grep -q "Please check Github"; then \
-		sed -i '1 i***Please check GitHub repo for latest version of slides: https://github.com/FZJ-JSC/tutorial-multi-gpu/ ***\n' README.md; \
-	fi;
 	sed -i '1 i***Please check GitHub repo for latest version of slides: https://github.com/FZJ-JSC/tutorial-multi-gpu/ ***\n' README.md
 	tar czf $@ --transform 's,^,SC22-tut102-Multi-GPU/,' --exclude=".*" $(SOURCES)
-# 	if grep -q "Please check Github"; then \
-		sed -i '2d' README.md; \
-	fi
+	tar xf $@
+	rm $@
+	find SC22-tut102-Multi-GPU/ -not -path './.*' -iname 'slides-*.pdf' -execdir rm slides.pdf \;
+	tar czf $@ SC22-tut102-Multi-GPU
+	rm -rf SC22-tut102-Multi-GPU
 	sed -i '1,2d' README.md

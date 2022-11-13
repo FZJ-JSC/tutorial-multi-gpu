1139308799fdcc9c610112985f78fa78efa9a375
Andreas Herten <a.herten@fz-juelich.de>
2022-11-13 11:29:10 -0600

    Change Nsight Systems module

--- .etc/modules.sh
+++ .etc/modules.sh
@@ -5,6 +5,6 @@ module load CUDA/11.7
 module load ParaStationMPI/5.6.0-1
 module load NCCL/default-CUDA-11.7
 module load NVSHMEM/2.5.0
-module load Nsight-Systems/2022.4.1
+module load Nsight-Systems/2022.2.1
 module load MPI-settings/CUDA
 # module use $JSCCOURSE_DIR_GROUP/common/modulefiles

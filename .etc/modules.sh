4d09c5ca66de0e71e7be1afd05184aa7cddc445d
Jiri Kraus <jkraus@nvidia.com>
2024-04-26 11:57:04 +0200

    Updates for 10H for ISC24 instance.

--- .etc/modules.sh
+++ .etc/modules.sh
@@ -1,10 +1,11 @@
 module purge
-module load Stages/2023
-module load GCC/11.3.0
-module load CUDA/11.7
-module load ParaStationMPI/5.7.1-1
-module load NCCL/default-CUDA-11.7
-module load NVSHMEM/2.8.0-CUDA-11.7
-module load Nsight-Systems/2023.2.1
+module use $OTHERSTAGES
+module load Stages/2024
+module load GCC/12.3.0
+module load CUDA/12
+module load ParaStationMPI/5.9.2-1
+module load NCCL/default-CUDA-12
+module load NVSHMEM/2.10.1-CUDA-12
+module load Nsight-Systems/2023.3.1
 module load MPI-settings/CUDA
 # module use $JSCCOURSE_DIR_GROUP/common/modulefiles

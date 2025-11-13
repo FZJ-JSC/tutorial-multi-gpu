91a9ebac64d6f1dd6fbaf2129dbd6821b5bede2d
Andreas Herten <a.herten@fz-juelich.de>
2025-11-13 14:05:30 +0100

    Update for SC

--- .etc/common-env/deploy.sh
+++ .etc/common-env/deploy.sh
@@ -1,5 +1,5 @@
 if [[ ! $PWD == *"common-env"* ]]; then
-	rsync --archive --exclude="deploy.sh" --exclude="raw/" --exclude="sc2*-titleslides/" --verbose . jureca.fz-juelich.de:/p/project/training2555/common/environment/
+	rsync --archive --exclude="deploy.sh" --exclude="raw/" --exclude="sc2*-titleslides/" --verbose . juwels-booster.fz-juelich.de:/p/project1/training2555/common/environment/
 else
 	echo "You seem to be in ./common-env/; please go up one dir."
 fi

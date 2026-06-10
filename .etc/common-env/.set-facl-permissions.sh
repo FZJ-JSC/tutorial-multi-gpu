c69598093aff13c1eb98a2521bf9608673465db2
Prateek Chawla <p.chawla@fz-juelich.de>
2026-06-10 12:10:56 +0200

    modify facl permissions
    Signed-off-by: Prateek Chawla <p.chawla@fz-juelich.de>

--- .etc/common-env/.set-facl-permissions.sh
+++ .etc/common-env/.set-facl-permissions.sh
@@ -2,9 +2,9 @@
 
 set -x
 
-course_project=${1:-$PROJECT_training2555}
+course_project=${1:-$PROJECT_training2630}
 
-for user in haghighimood1 kraus1 hrywniak1 oden1 garciadegonzalo1 badwaik1 john2 appelhans1; do
+for user in chawla2 rajski1 dabah2 herten1 ; do
 	setfacl -m u:$user:rwx -R $course_project/common/
 	setfacl -m u:$user:rwx -R $course_project/env.sh
 done

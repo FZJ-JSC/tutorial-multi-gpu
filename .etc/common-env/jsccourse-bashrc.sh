6fa39efac1a252d82d04656dbab67c6e0e25fcab
Prateek Chawla <p.chawla@fz-juelich.de>
2026-06-10 12:14:32 +0200

    change dates, reservations and modules
    Signed-off-by: Prateek Chawla <p.chawla@fz-juelich.de>

--- .etc/common-env/jsccourse-bashrc.sh
+++ .etc/common-env/jsccourse-bashrc.sh
@@ -15,7 +15,7 @@
 
 
 if [ -z "$_JSCCOURSE_ENV_SOURCED" ]; then
-	project="training2502"
+	project="training2630"
 	_JSCCOURSE_PROJECT=${JSCCOURSE_PROJECT:-$project}
 	_JSCCOURSE_SHORTNAME=${JSCCOURSE_SHORTNAME:-"GPU-Course"}
 	_JSCCOURSE_HUMANNAME=${JSCCOURSE_HUMANNAME:-"GPU Course"}
@@ -29,8 +29,8 @@ if [ -z "$_JSCCOURSE_ENV_SOURCED" ]; then
 	jutil env activate -p $JSCCOURSE_PROJECT -A $JSCCOURSE_PROJECT
 
 	currentday=$(date +%d)
-	if [[ "$currentday" == "16" ]]; then
-		res="--reservation sc25-multigpu-tut"
+	if [[ "$currentday" == "17" ]] || [[ "$currentday" == "18" ]] || [[ "$currentday" == "19" ]]; then
+		res="--reservation summerschool"
 	fi
 	
 	export SLURM_NTASKS=1
@@ -102,7 +102,7 @@ if [ -z "$_JSCCOURSE_ENV_SOURCED" ]; then
 fi
 
 if [[ $- =~ "i" ]]; then
-	for script_rel in modules.sh; do
+	for script_rel in modules_jupiter.sh; do
 		abs_script=$__SCRIPT_DIR/$script_rel
 		if [ -e $abs_script ]; then
 			source $abs_script

16c0535e70e92fae0ce1c3cad774089a630827c6
Prateek Chawla <p.chawla@fz-juelich.de>
2026-06-15 08:46:03 +0200

    fix jupiter login node
    Signed-off-by: Prateek Chawla <p.chawla@fz-juelich.de>

--- .etc/common-env/deploy.sh
+++ .etc/common-env/deploy.sh
@@ -1,5 +1,5 @@
 if [[ ! $PWD == *"common-env"* ]]; then
-	rsync --archive --exclude="deploy.sh" --exclude="raw/" --exclude="hpc-ai*-titleslides/" --verbose . jupiter.fz-juelich.de:/e/project1/training2630/common/environment/
+	rsync --archive --exclude="deploy.sh" --exclude="raw/" --exclude="hpc-ai*-titleslides/" --verbose . login.jupiter.fz-juelich.de:/e/project1/training2630/common/environment/
 else
 	echo "You seem to be in ./common-env/; please go up one dir."
 fi
--- .etc/deploy-material.sh
+++ .etc/deploy-material.sh
@@ -1,5 +1,5 @@
 if [[ ! $PWD == *"env"* ]]; then
-	rsync --archive --exclude="*minified.pdf" --exclude="tut*" --exclude=".*" --exclude="*-sc*.pdf"  --verbose .. jupiter.fz-juelich.de:/e/project1/training2630/common/material/
+	rsync --archive --exclude="*minified.pdf" --exclude="tut*" --exclude=".*" --exclude="*-sc*.pdf"  --verbose .. login.jupiter.fz-juelich.de:/e/project1/training2630/common/material/
 else
 	echo "You seem to be in ./env/; please go up one dir."
 fi

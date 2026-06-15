dcb6524e55f6d664fcabfbcbabd31fde5f463409
Prateek Chawla <p.chawla@fz-juelich.de>
2026-06-15 08:50:13 +0200

    add MB's user
    Signed-off-by: Prateek Chawla <p.chawla@fz-juelich.de>

--- .etc/common-env/.set-facl-permissions.sh
+++ .etc/common-env/.set-facl-permissions.sh
@@ -4,7 +4,7 @@ set -x
 
 course_project=${1:-$PROJECT_training2630}
 
-for user in chawla2 rajski1 dabah2 herten1 ; do
+for user in chawla2 rajski1 dabah2 bode5 herten1 ; do
 	setfacl -m u:$user:rwx -R $course_project/common/
 	setfacl -m u:$user:rwx -R $course_project/env.sh
 done
